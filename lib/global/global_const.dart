9c402e839d8cfe364d9daaf7bb4c2d3836da2953
huyk21 <92777449+huyk21@users.noreply.github.com>
2024-04-29 22:08:37 +0700

    fix minor bug

--- lib/global/global_const.dart
+++ lib/global/global_const.dart
@@ -13,7 +13,7 @@ const CameraPosition MapboxPlexInitialPosition = CameraPosition(
 
 String userPhone = "";
 String userID = FirebaseAuth.instance.currentUser!.uid;
-String serverKeyFCM = "AAAAweGYj8c:APA91bHPCnM4FG3KMn_jc1lyZhE1aPTKW6p1-twv2PeO2Qkd6AbyGf3vnTfhP-bj5B-U7nX7flyr9clyc9XH2D15I7oygMScSj4MV5-GIpTzkzCcLPb5B9TJGh1BSVJ51DCPOdjTRcWq";
+String serverKeyFCM = "key=AAAAweGYj8c:APA91bHPCnM4FG3KMn_jc1lyZhE1aPTKW6p1-twv2PeO2Qkd6AbyGf3vnTfhP-bj5B-U7nX7flyr9clyc9XH2D15I7oygMScSj4MV5-GIpTzkzCcLPb5B9TJGh1BSVJ51DCPOdjTRcWq";
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
   target: LatLng(37.42796133580664, -122.085749655962),
